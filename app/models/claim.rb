6ea3c0e14509ff3719ed8e751b4b9d4ec1d29b5c
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-07 15:56:07 +0100

    2.5. Added styling for status colour background of claim_id on claims index.

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -2,7 +2,7 @@ class Claim
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :status, type: String, default: 'Open'
+  field :status, type: String, default: 'open'
   field :claim_id, type: String
   field :claimant_name, type: String
   field :postcode, type: String
