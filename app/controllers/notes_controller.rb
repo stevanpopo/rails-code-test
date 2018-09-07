d24cf4fd4448cd892702f4ee618c8adb761b85c3
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-07 18:50:52 +0100

    3.2. Had to add before_action to set claim on notes controller.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,16 @@
 class NotesController < ApplicationController
+  before_action :set_claim
 
   # GET /notes
   # GET /notes.json
   def index
     @notes = @claim.notes
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_claim
+      @claim = Claim.find(params[:claim_id])
+    end
 
 end
