adee3ff3602c5729433969975fefd7e4993c835e
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-07 14:44:25 +0100

    2.1. Added settlement_amount and settlement_date to claim model, form and locales (for human names)

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,11 +1,13 @@
 class Claim
-  include Mongoid::Document 
+  include Mongoid::Document
   include Mongoid::Timestamps
 
   field :claim_id, type: String
   field :claimant_name, type: String
   field :postcode, type: String
   field :policy_no, type: String
+  field :settlement_amount, type: String
+  field :settlement_date, type: String
 
   has_many :emails
 end
