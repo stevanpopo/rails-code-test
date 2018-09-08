999cf22363f890d6d6aa471dc320d50c204a4a09
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-08 10:45:48 +0100

    3.5. Added destory action in notes controller. Connected up delete button so it works. Redirect return to claim.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -39,6 +39,16 @@ class NotesController < ApplicationController
     end
   end
 
+  # DELETE /emails/1
+  # DELETE /emails/1.json
+  def destroy
+    @note.destroy
+    respond_to do |format|
+      format.html { redirect_to @claim }
+      format.json { head :no_content }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_claim_and_note
